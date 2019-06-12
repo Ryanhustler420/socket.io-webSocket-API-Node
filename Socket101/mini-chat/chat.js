7359498a8dfcce59399713e292add93be270bc71
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-12 20:44:18 +0530

    Refector

--- Socket101/mini-chat/chat.js
+++ Socket101/mini-chat/chat.js
@@ -6,42 +6,20 @@ app.use(express.static(__dirname + '/public'));
 
 const expressServer = app.listen(9000);
 
-// const io = scoketio(expressServer);
-
-// OR
-
 const io = scoketio();
 io.attach(expressServer);
 
-// io.on == io.of('/').on
-
-// Main Namespace
 io.on('connection', socket => {
   socket.emit('messageFromServer', {data: 'Hey Client'});
   socket.on('dataToServer', dataFromClient => {
     console.log(dataFromClient);
   });
-  socket.on('newMessageToServer', msg => {
-    io.emit('messageToClient', {text: msg.text});
-    io.of('/').emit('messageToClient', {text: msg.text}); //<- as same as above code
-  });
-
-  // The server can still communicate across namespace
-  // but on the clientInformation, the socket needs to be in THAT namespace
-  // in order to get the events
-
-  // Dont use this
-  //   The root socket has all the namesapce contacts
-
-  //   setTimeout(() => {
-  //     io.of('/admin').emit(
-  //       'welcome',
-  //       'Welcome to the admin channle, form the root channel!'
-  //     );
-  //   }, 2000);
 });
 
+// Only for /admin namespace
 io.of('/admin').on('connection', socket => {
-  console.log('Someone connected to the admin namespace');
-  io.of('/admin').emit('welcome', 'Welcome to the admin channel!');
+  socket.emit('welcome', 'Welcome to the admin channel!');
+  socket.on('fuck', data => {
+    console.log(data);
+  });
 });
--- Socket101/mini-chat/public/scripts.js
+++ Socket101/mini-chat/public/scripts.js
@@ -1,34 +1,20 @@
-const rootSocket = io('http://localhost:9000');
-// this is the endpoint as root `/` as default [namespace]
-
-const socket2 = io('http://localhost:9000/admin');
+const rootSocket = io('http://localhost:9000'); // Main namespace
+const adminSocket = io('http://localhost:9000/admin'); // admin namespace
 // this is the endpoint as `/admin` [namespace]
 
 rootSocket.on('messageFromServer', dataFromServer => {
-  console.log(rootSocket.id);
   console.log(dataFromServer);
   rootSocket.emit('dataToServer', {data: 'Data from the Client'});
 });
 
-socket2.on('connect', () => {
-  console.log(socket2.id);
+adminSocket.on('welcome', datafromserver => {
+  console.log(datafromserver);
 });
 
-socket2.on('welcome', msg => {
-  console.log(msg);
-});
-
-// this is not in /admin namespace so it won't works
-rootSocket.on('welcome', msg => {
-  console.log(msg);
-});
+adminSocket.emit('fuck', {data: 'Client Wants To Fuck The Server!'});
 
 document.querySelector('#message-form').addEventListener('submit', e => {
   e.preventDefault();
   const newMessage = document.querySelector('#user-message').value;
   rootSocket.emit('newMessageToServer', {text: newMessage});
-});
-
-rootSocket.on('messageToClient', msg => {
-  document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`;
 });
